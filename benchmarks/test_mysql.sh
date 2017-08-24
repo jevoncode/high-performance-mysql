d575bbf2498e06b838fc9a3113d6763db69e15a2
jevoncode <jevoncode@gmail.com>
2017-08-24 18:33:51 +0800

    add analyze

--- benchmarks/test_mysql.sh
+++ benchmarks/test_mysql.sh
@@ -2,8 +2,8 @@
 
 INTERVAL=5
 PREFIX=$INTERVAL-sec-status
-RUNFILE=/home/benchmarks/running
-mysql -e 'SHOW GLOBAL VARIABLES' >> mysql-variables
+RUNFILE=/home/grande/mysql-study/high-performance-mysql/benchmarks/running
+mysql -uroot -proot -e 'SHOW GLOBAL VARIABLES' >> mysql-variables
 while test -e $RUNFILE; do
 	file=$(date +%F_%I)
 	sleep=$(date +%s.%N | awk "{print $INTERVAL - (\$1 % $INTERVAL)}")
@@ -11,11 +11,11 @@ while test -e $RUNFILE; do
 	ts="$(date +"TS $s.%N %F %T")"
 	loagavg="$(uptime)"
 	echo "$ts $loadavg" >> $PREFIX-${file}-status
-	mysql -e 'SHOW GLOBAL STATUS' >> $PREFIX-${file}-status &
+	mysql -uroot -proot -e 'SHOW GLOBAL STATUS' >> $PREFIX-${file}-status &
         echo "$ts $loadavg" >> $PREFIX-${file}-innodbstatus
-        mysql -e 'SHOW ENGINE INNODB STATUS\G' >> $PREFIX-${file}-innodbstatus &
+        mysql -uroot -proot -e 'SHOW ENGINE INNODB STATUS\G' >> $PREFIX-${file}-innodbstatus &
         echo "$ts $loadavg" >> $PREFIX-${file}-processlist
-        mysql -e 'SHOW FULL PROCESSLIST\G' >> $PREFIX-${file}-processlist &
+        mysql -uroot -proot -e 'SHOW FULL PROCESSLIST\G' >> $PREFIX-${file}-processlist &
 	echo $ts
 done
 echo Exitin because $RUNFILE does not exist.
